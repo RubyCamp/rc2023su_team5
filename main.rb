3ca3652584778b8cedf5614816119152989b9fe4
toyoshima <takutaku3230gudakusan@gmail.com>
2023-08-24 16:26:27 +0900

    DXruby上でmapを表示するコードを作成

--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
-# main.rbとあるが最初の真っ白なmap上をキー入力で動くようにしているコードである
+# DXruby上にmapを表示するコード
 
 require 'dxruby'
 require_relative 'navigator'
+require_relative 'carrier'
 
 navigator = Navigator.new
+carrier = Carrier.new
+carrier.search_map
 
 font = Font.new(15)
+navigator_route = [:b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l]
 
 wakus = {
     :a =>  Sprite.new(180, 120, Image.new(50, 50, C_WHITE)),
@@ -45,9 +49,15 @@ color_mapping = {
     6.0 => C_WHITE,
 }
 
- # 
- # color = color_mapping[sensor_value]
- # wakus[:a].image = Image.new(50, 50, color)
+ # mapの画像を変更する処理
+navigator_route.each.with_index do |node_name, i|
+    # 1. マスの色をcarrier.node_colors[i]から取得
+    sensor_color = carrier.node_colors[i]
+    # 2. 1.の情報をcolor_mappingでカラー定数に変換
+    color = color_mapping[sensor_color]
+    # 3. wakusのnode_nameのSpriteを2.のカラー定数のImageで更新(Sprite#image=で更新)
+    wakus[node_name].image = Image.new(50, 50, color)
+end
 
 Window.loop do 
     break if Input.key_push?(K_ESCAPE)
